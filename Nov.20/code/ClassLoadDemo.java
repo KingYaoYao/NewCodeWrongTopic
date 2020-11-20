8d188b026cd099271267f174c68c33b392e46fed
3071980057@qq.com <3071980057@qq.com>
2020-11-20 17:05:39 +0800

    Nov20-code

---
+++ Nov.20/code/ClassLoadDemo.java
@@ -0,0 +1,20 @@
+public class ClassLoadTest {
+    public static void main(String[] args) {
+        new B();
+    }
+}
+class A {
+    public A() {    //父类构造器
+        System.out.println("class A");
+    }
+    { System.out.println("I'm A class"); }      //父类代码块
+    static { System.out.println("class A static"); }    //父类静态代码块
+}
+ class B extends A {
+    public B() {    //子类构造器
+        System.out.println("class B");
+    }
+    { System.out.println("I'm B class"); }      //子类代码块
+    static { System.out.println("class B static"); }    //子类静态代码块
+
+}
